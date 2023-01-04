cae69785179e0f437d3a73843f068a14b17fe58a
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-04 22:11:16 +0530

    [amol]- Calculate Daily Employee Wag add Part Time

--- EmployeeWag/EmployeeWag/Program.cs
+++ EmployeeWag/EmployeeWag/Program.cs
@@ -10,14 +10,19 @@ namespace EmployeeWag
     {
         static void Main(string[] args)
         {
-            int fullTime = 1, EmpRatePerHour = 20, empHrs = 0, empWage = 0;
+            int fullTime = 1, PartTime = 2, EmpRatePerHour = 20, empHrs = 0, empWage = 0;
             Random randobj = new Random();
-            int checkPresent = randobj.Next(0, 2);
+            int checkPresent = randobj.Next(0, 3);
             Console.WriteLine("Random number=" + checkPresent);
             if (checkPresent == fullTime)
             {
                 empHrs = 8;
                 Console.WriteLine("Employee Is Present");
+            }
+            else if (checkPresent == PartTime)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is part time");
             }
             else
             {
