7bc76d1e38957d97dcf5ee4d436dcaeb3850efc8
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-04 22:02:58 +0530

    [amol]- UC1_Check Employee Present Or Absent

--- EmployeeWag/EmployeeWag/Program.cs
+++ EmployeeWag/EmployeeWag/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fullTime = 1;
+            Random randobj = new Random();
+            int checkPresent = randobj.Next(0, 2);
+            Console.WriteLine("Random number=" + checkPresent);
+            if (checkPresent == fullTime)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+
+        }
+     }
+}
