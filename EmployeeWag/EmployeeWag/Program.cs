c50932b343f46729345213a0ca3e30aa3ac3e1cf
Amol <ajamnik.mech@gmail.com>
2023-01-22 22:27:50 +0530

    [Amol]- Store the Daily Wage along with the Total Wage

--- EmployeeWag/EmployeeWag/Program.cs
+++ EmployeeWag/EmployeeWag/Program.cs
@@ -11,14 +11,14 @@ namespace EmployeeWag
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Employee Wage Calculation Program");
-            //WageComputation attendancecheck = new WageComputation();
-            //attendancecheck.EmployeeAttendence();
-            WageComputation empwagecomputation = new WageComputation(2);
-            empwagecomputation.AddCompany("Reliance", 20, 8, 4, 100, 20);
-            empwagecomputation.EmpWageCalculation("reliance");
-            empwagecomputation.AddCompany("Tata", 25, 9, 5, 90, 24);
-            empwagecomputation.EmpWageCalculation("tata");
-            empwagecomputation.DisplayWage();
+            WageComputation employeeComputation = new WageComputation(2);
+            employeeComputation.AddCompany("TATA", 20, 8, 4, 100, 20);
+            employeeComputation.EmpWageCalculation("TATA");
+
+            employeeComputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
+            employeeComputation.EmpWageCalculation("MAHINDRA");
+
+            employeeComputation.DisplayWage();
         }
     }
 }
